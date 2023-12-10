e98391b3b9c032ace2ee656cf802a4f484075b7d
amarpriyesh <63479437+amarpriyesh@users.noreply.github.com>
2023-12-10 06:19:45 -0500

    Images Fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['priyeshportfolio.info'],
-    loader: 'default',
-    path: 'https://priyeshportfolio.info/',
-  },
+ 
 
 };
 
