4127f9decef85e725a565de89a862785ecdb5bab
amarpriyesh <63479437+amarpriyesh@users.noreply.github.com>
2023-12-10 07:01:56 -0500

    Images Fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port =
 
 console.log(port);
 const dev = process.env.NEXT_ENV !== "production";
-const app = next({ dev, port});
+const app = next({ dev});
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
