eeedcb9c17e31fce31c509bc2e9b7503f8e7f974
amarpriyesh <63479437+amarpriyesh@users.noreply.github.com>
2023-12-10 06:30:15 -0500

    Images Fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
- 
-
+  images: {
+    domains: ["priyeshportfolio.info", "127.0.0.1"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "priyeshportfolio.info",
+        port: "443",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "3000",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
