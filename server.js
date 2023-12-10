b93b0b2c19dc93d6f01d7193b8cd5e22e75c3211
amarpriyesh <63479437+amarpriyesh@users.noreply.github.com>
2023-12-10 04:58:28 -0500

    SSL Changes

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const { createServer } = require("http");
+const { createServer: createHttpsServer } = require("https");
+const next = require("next");
+const fs = require("fs");
+require("dotenv").config();
+
+const port =
+  process.env.NEXT_ENV === "development"
+    ? process.env.NEXT_PORT_DEV
+    : process.env.NEXT_PORT_PROD;
+
+console.log(port);
+const dev = process.env.NEXT_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.get("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  if (dev) {
+    // In development, use HTTP
+    createServer(server).listen(port, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${port}`);
+    });
+  } else {
+    // In production, use HTTPS
+    const httpsOptions = {
+      key: fs.readFileSync('/root/key/priyeshportfolio.info.key'),
+      cert: fs.readFileSync('/root/key/priyeshportfolio.info.pem'),
+    };
+
+    createHttpsServer(httpsOptions, server).listen(port, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on https://localhost:${port}`);
+    });
+  }
+});
