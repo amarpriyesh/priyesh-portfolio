be30d994dd4a71aea8b379a5e77a7377da45b609
amarpriyesh <63479437+amarpriyesh@users.noreply.github.com>
2023-12-21 13:22:00 -0500

    implementation of Store

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,12 @@
-import '@/styles/globals.css'
-import '@/styles/ExploreSlider.css'
-import { Provider } from 'react-redux';
-import store from './store'; 
+import "@/styles/globals.css";
+import "@/styles/ExploreSlider.css";
+import { Provider } from "react-redux";
+import store from "../components/store";
 
 export default function App({ Component, pageProps }) {
-
   return (
     <Provider store={store}>
-  <Component {...pageProps} />
-  </Provider>
-  )
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
